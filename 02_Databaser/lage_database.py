4fce006ba21cdf7d21a455ccbeef83958ec41c77
Jørgen Navjord <navjordj@gmail.com>
2020-11-11 21:37:41 +0100

    start på opprettelse av sql database

---
+++ 02_Databaser/lage_database.py
@@ -0,0 +1,20 @@
+import sqlite3 # Biblioteket vi skal bruke for å kommunisere med sqlite-databasen
+from sqlite3 import Error
+
+connection = sqlite3.connect("database.db") # 
+
+if connection is not None: # Sjekker om opprettelsen av databasen var en suksess
+    sql_create_table =  "CREATE TABLE IF NOT EXISTS sensordata (id integer PRIMARY KEY, value integer NOT NULL); "
+    try:
+        c = connection.cursor()
+        c.execute(sql_create_table)
+    except Error as e:
+        print(e)
+
+
+else:
+    print("En feil oppsto under opprettelsen av databasen")
+
+
+
+connection.close() # Viktig å lukke databasen før programmet avslutter
