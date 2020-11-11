e498413520447079fc1330d00d44d4861104ca50
Jørgen Navjord <navjordj@gmail.com>
2020-11-11 21:59:39 +0100

    setter in 10 flyttall i tabellen

--- 02_Databaser/innsetting_av_data.py
+++ 02_Databaser/innsetting_av_data.py
@@ -1,11 +1,18 @@
 import sqlite3 
 from sqlite3 import Error 
 
+import random
+
 connection = sqlite3.connect("database.db")
 
-sql_insert = "INSERT INTO sensordata (value) VALUES (1)"
+sql_insert = "INSERT INTO sensordata (value) VALUES (0.7)"
 
 c = connection.cursor()
 c.execute(sql_insert)
+
+for i in range(9):
+    sql_insert = f'INSERT INTO sensordata (value) VALUES ({round(random.random(), 2)})'
+    c.execute(sql_insert)
+
 connection.commit()
 connection.close()
